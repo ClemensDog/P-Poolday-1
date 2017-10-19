7d2b9367f8c0287f587d062bf336084ca47b0cfa
Clemens Koch <clemdogs@persocomcchi.kea.local>
2017-10-19 12:54:59 +0200

    nr 3

--- scribt.js
+++ scribt.js
@@ -1,6 +1,10 @@
 let hund = document.querySelector(".puppy");
+let splat = document.querySelector(".blood");
+let anders = document.querySelector(".hvalp");
+let avfor = document.querySelector(".av");
 
 hund.addEventListener('mouseover', flygt);
+hund.addEventListener('click', die);
     console.log("sker der noget hallo?");
 
 
@@ -9,4 +13,15 @@ function flygt() {
     let y = Math.floor(Math.random() * 600);
 
     hund.style.transform = "translate(" + x + "px, " + y + "px)";
+}
+
+
+function die(){
+    console.log("hejsa den viker");
+    splat.classList.toggle("blood");
+    splat.style.opacity(1);
+    anders.style.opacity(0);
+    console.log("Av Siger Anders");
+    avfor.play();
+
 }
